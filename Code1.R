0e620d6c99015221d9136328be7e3f1f1cb9923d
luke-loken <lukeloken@gmail.com>
2016-07-27 17:02:43 -0500

    Coded picarro read and time conversions

--- Code1.R
+++ Code1.R
@@ -1,3 +1,30 @@
 #Code that does stuff
 
 #More code that does other stuff
+library(R.matlab)
+library(gdata)
+
+setwd("E:/Dropbox/FLAME_ColumbiaRiver/picarro_data")
+
+setwd("matlab")
+
+files<-list.files()
+
+pic1<-readMat(files[1])
+
+setwd("..")
+setwd("csv")
+
+files<-list.files()
+
+pic1<-read.table(files[2], header=T, sep=",", stringsAsFactors = F)
+head(pic1[,1:5])
+
+pic1$DateTime<-as.POSIXct(paste(pic1$DATE, floor((pic1$TIME*24)), pic1$min, pic1$sec, sep=" "), format="%Y-%m-%d %H %M %S", tz="UTC")
+
+head(pic1$DateTime)
+pic1<-pic1[order(pic1$DateTime),]
+
+plot(pic1$DateTime, pic1$HR_12CH4_dry)
+
+
