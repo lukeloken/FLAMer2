b7b6e67c4954fc2bd591b0bdb74fef3519d00700
luke-loken <lukeloken@gmail.com>
2016-07-28 11:05:10 -0500

    Incoroporated picarro data in read_instruments flame work flow. Code will now save and plot picarro data if subfolder 'picarro' has any files.

--- Code1.R
+++ Code1.R
@@ -11,23 +11,28 @@ Picarro_Time_Offset<-15
 #Load Picarro directory and list all files
 setwd("E:/Dropbox/FLAME_ColumbiaRiver")
 dir<-("picarro_data/csv")
-PicarroPath<-list.files(dir)
 
 #Bind all files in above path using read.table - Need to specify skip, header, etc. 
-if (length(PicarroPath)>0){
-  PicarroFull<-do.call("rbind", lapply(paste(PicarroPath, sep=""), read.table, sep="," ,skip=0,  header = TRUE, fill=TRUE)) 
-  
-  PicarroFull$DateTime_UTC<-as.POSIXct(paste(PicarroFull$DATE, floor((PicarroFull$TIME*24)), PicarroFull$min, PicarroFull$sec, sep=" "), format="%Y-%m-%d %H %M %S", tz="UTC")
-  PicarroFull<-PicarroFull[order(PicarroFull$DateTime_UTC),]
-  
-  PicarroFull=PicarroFull[!duplicated(PicarroFull[,"DateTime_UTC"]),]
 
-  PicarroFull$DateTime_UTC<-(PicarroFull$DateTime_UTC+Picarro_Time_Offset[1])
+PicarroRead<-function(dir, Picarro_Time_Offset){
+  PicarroPath<-list.files(dir)
+  if (length(PicarroPath)>0){
+    PicarroFull<-do.call("rbind", lapply(paste(dir, "/", PicarroPath, sep=""), read.table, sep="," ,skip=0,  header = TRUE, fill=TRUE)) 
   
-  Picarrovars <- c(68, 26:45)
-  PicarroFull <- PicarroFull[,Picarrovars]
+    PicarroFull$DateTime_UTC<-as.POSIXct(paste(PicarroFull$DATE, floor((PicarroFull$TIME*24)), PicarroFull$min, PicarroFull$sec, sep=" "), format="%Y-%m-%d %H %M %S", tz="UTC")
+    PicarroFull<-PicarroFull[order(PicarroFull$DateTime_UTC),]
+  
+    PicarroFull=PicarroFull[!duplicated(PicarroFull[,"DateTime_UTC"]),]
 
+    PicarroFull$DateTime_UTC<-(PicarroFull$DateTime_UTC+Picarro_Time_Offset[1])
+  
+    Picarrovars <- c(68, 26:45)
+    PicarroFull <- PicarroFull[,Picarrovars]
+
+    return(PicarroFull)
+  }
 }
+
 
 
 
---
+++ RUN_FLAME_2016_Picarro.R
@@ -0,0 +1,157 @@
+############################################################################################
+rm(list=ls(all=TRUE)) #clear the environment
+ 
+# ==================================================
+# Change directory to match desired dataset
+# Change date and sitename!
+# Should read ("/Dropbox/.../YYYY-MM-DD_SiteName)
+# Added a loop function to do this for all 6 ColumbiaRiver Days. Remove loop if wanting to run a single day.
+# ==================================================
+
+FLAME_Data_Directory<-"Dropbox/FLAME_ColumbiaRiver/Data/2016-07-12_ColumbiaRiver_Day1"
+
+#Enter Picarro time offset in seconds
+Picarro_Time_Offset<-15
+
+# ==================================================
+# Point to the main FLAME dropbox directory
+# This should be your "yourcomputer"/Dropbox/FLAME
+# ==================================================
+
+setwd("E:/Dropbox/FLAME") #Loken Desk
+
+# Other computers
+# setwd("F:/Dropbox/FLAME") #?
+# setwd("C:/Users/jtcrawford/Dropbox/FLAME") #Crawford Laptop
+# setwd("C:/Users/Luke/Dropbox/FLAME") #Loken Laptop
+
+# ==============================
+# Don't change anything below!
+# Script uses relative directories within "Dropbox/FLAME"
+# ==============================
+
+setwd("R scripts")
+
+# call the FLAMe scripts
+source("FLAMer.R")
+
+# Load tau corrections
+setwd("..")
+setwd("Taus")
+
+taus2015.01<-read.csv("Manual_Hydros_Taus_2015.csv", header=TRUE)
+taus2016.01<-read.csv("Manual_Hydros_Taus_2016.01.csv", header=TRUE)
+taus2016.02<-read.csv("Manual_Hydros_Taus_2016.02.csv", header=TRUE)
+taus2016.03<-read.csv("Manual_Hydros_Taus_2016.03.csv", header=TRUE)
+
+# Load rule table for sensorQC
+setwd("..")
+setwd("SensorQC")
+
+ruletable<-read.csv("SensorQCRulesStrick2_2015.csv", header=TRUE, stringsAsFactors=FALSE)
+
+# Add common basemaps
+setwd("..")
+setwd("basemaps")
+
+Mendota_Base<-readOGR(getwd(), "Mendota_shape")
+Mendota_Base_UTM<-spTransform(Mendota_Base, CRS("+proj=utm +zone=15 ellps=WGS84"))
+Pool8_Base_UTM<-readOGR(getwd(), "Pool8")
+Pool8_Base<-spTransform(Pool8_Base_UTM, CRS("+init=epsg:4326"))
+lakes_Base_UTM<-readOGR(getwd(), "regional_lakes_3")
+lakes_Base<-spTransform(lakes_Base_UTM, CRS("+init=epsg:4326"))
+UNDERC_Base_UTM<-readOGR(getwd(), "Peter_Paul_Tuesday_shifted2")
+UNDERC_Base<-spTransform(UNDERC_Base_UTM, CRS("+init=epsg:4326"))
+
+# Read in zerotimes from Peter Turner
+setwd("..")
+setwd("Ambient_Times")
+
+zerotimes<-read.csv("Ambient_Times_2015.csv", header=TRUE)
+zerotimes[,1]<-as.POSIXct(strptime(as.character(zerotimes[,1]), format="%m/%d/%Y %H:%M"), tz="America/Chicago")
+zerotimes[,2]<-as.POSIXct(strptime(as.character(zerotimes[,2]), format="%m/%d/%Y %H:%M"), tz="America/Chicago")
+
+# point to the data folder; individual instrument files need to be in subfolders 
+# with the names 'laser', 'gps', 'ysi' 'nitro', 'meta', 'samples' (case sensitive)
+
+setwd("..")
+setwd("..")
+setwd("..")
+setwd(paste(getwd(), FLAME_Data_Directory, sep=""))
+
+# ===========================================================
+# Start Open UMR Loop Code
+# Loop code, here and an after flame_engage function
+# Code will loop through all 13 UMR files and perform {flame engage}
+# starting directory - setwd("C:/Users/Luke/Dropbox/FLAME")
+# ===========================================================
+
+# for (days in 1:13){
+#   if (days<10){
+#     FLAMEsubdirectory<-(paste("/Data/2015-08-0", days, "_UMR_Day", days, sep=""))}
+#   if (days>=10){
+#     FLAMEsubdirectory<-(paste("/Data/2015-08-", days, "_UMR_Day", days, sep=""))}  
+#   
+#   setwd("..")
+#   setwd("..")
+#   setwd(paste(getwd(), FLAMEsubdirectory, sep=""))
+
+# ===========================================================
+# End Open UMR All Days - Loop Code above
+# ===========================================================
+  
+# read in a metadata file 
+# indicates the date (YYYY-MM-DD !!!), site, instruments, flame intervals, and water samples
+MetaPath <- list.files(path = paste(getwd(), "/meta", sep=""))
+meta<-do.call("rbind", lapply(paste("meta/", MetaPath, sep=""), read.table, sep="," ,skip=0,  header = TRUE, fill=TRUE)) 
+Date<-as.Date(meta$Date[1], format="%Y-%m-%d")
+Site<-as.character(meta$Site[1])
+
+
+if (meta$number_of_samples[1]!=0){
+SamplesPath <- list.files(path = paste(getwd(), "/samples", sep=""))
+Samples<-do.call("rbind", lapply(paste("samples/", SamplesPath, sep=""), read.table, sep="," ,skip=0,  header = TRUE, fill=TRUE))}
+
+if (meta$number_of_atm_samples[1]>0){
+  AtmPath <- list.files(path = paste(getwd(), "/atm", sep=""))
+  Atm<-do.call("rbind", lapply(paste("atm/", AtmPath, sep=""), read.table, sep="," ,skip=0,  header = TRUE, fill=TRUE))}
+
+# Set correct basemap
+if(Site=="Peter" | Site=="Paul" | Site=="Tuesday" |Site=="TuesdayLake" | Site=="PeterLake" |Site=="PaulLake"){
+  base<-UNDERC_Base
+  } else if(Site=="LakeMendota"){
+  base<-Mendota_Base
+  } else if(Site=="Pool8"){
+  base<-Pool8_Base
+  } else {
+  base<-lakes_Base
+  }
+
+# Set correct Tau Conversion
+if (meta$FLAME_Unit[1]=="2015.01"){
+  taus<-taus2015.01}
+if (meta$FLAME_Unit[1]=="2016.01"){
+  taus<-taus2016.01}
+if (meta$FLAME_Unit[1]=="2016.02"){
+  taus<-taus2016.02}
+if (meta$FLAME_Unit[1]=="2016.03"){
+  taus<-taus2016.03}
+
+
+# ===========================================================
+# do all 'dem bad things to the FLAMe data
+# ===========================================================
+
+flame_engage_all_maps(meta)
+
+# ===========================================================
+# Start close UMR loop code - see above
+# ===========================================================
+
+# print(FLAMEsubdirectory)
+# }
+  
+# ===========================================================
+# End close loop code
+# ===========================================================
+  
